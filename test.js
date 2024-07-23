314927d6f3ffc0c78085058da26afafd83948dc1
Shreya <shreya.adh09@gmail.com>
2024-07-24 00:13:50 +0530

    Initial commit with readme file

--- test.js
+++
@@ -1,9 +0,0 @@
-const generateQRCode = require('./index');
-
-generateQRCode('Hello, world!', 'qrcode.png', (err, message) => {
-    if (err) {
-        console.error('Error:', err);
-    } else {
-        console.log(message);
-    }
-});
